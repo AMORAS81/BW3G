b48ee6c833526bf2f39eef040abb453e7a6cfb86
AMORAS81 <markamoras81@gmail.com>
2025-01-30 15:02:37 -0300

    gabite.asm

--- data/pokemon/base_stats/vibrava.asm
+++ data/pokemon/base_stats/vibrava.asm
@@ -1,21 +1,21 @@
-	db VIBRAVA ; 183
+	db GABITE ; 183
 
-	db  50,  70,  50,  70,  50,  50
+	db  68,  90,  65,  82,  50,  55
 	;   hp  atk  def  spd  sat  sdf
 
-	db GROUND, DRAGON ; type
-	db 120 ; catch rate
-	db 126 ; base exp
+	db DRAGON, GROUND ; type
+	db 45 ; catch rate
+	db 144 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
-	db 20 ; step cycles to hatch
+	db 40 ; step cycles to hatch
 	db 5 ; unknown 2
-	INCBIN "gfx/pokemon/vibrava/front.dimensions"
+	INCBIN "gfx/pokemon/gabite/front.dimensions"
 	db 0, 0, 0, 0 ; padding
-	db GROWTH_MEDIUM_SLOW ; growth rate
-	dn EGG_BUG, EGG_BUG ; egg groups
+	db GROWTH_SLOW ; growth rate
+	dn EGG_MONSTER, EGG_DRAGON ; egg groups
 
 	; tm/hm learnset
-	tmhm TOXIC, HIDDEN_POWER, SUNNY_DAY, HYPER_BEAM, PROTECT, SOLARBEAM, EARTHQUAKE, RETURN, DIG, BULLDOZE, DOUBLE_TEAM, X_SCISSOR, SANDSTORM, ROCK_TOMB, REST, ATTRACT, ROCK_SLIDE, STRUGGLE_BUG, CUT, FLY, STRENGTH, STEEL_WING, OUTRAGE, SIGNAL_BEAM, DRAGON_PULSE, BUG_BITE, GIGA_DRAIN, HYPER_VOICE, EARTH_POWER
+	tmhm DRAGON_CLAW, TOXIC, HIDDEN_POWER, SUNNY_DAY, PROTECT, RAIN_DANCE, STONE_EDGE, EARTHQUAKE, RETURN, DIG, BULLDOZE, DOUBLE_TEAM, FLAMETHROWER, SANDSTORM, FIRE_BLAST, ROCK_TOMB, SHADOW_CLAW, REST, ROCK_SLIDE, CUT, STRENGHT, IRON_TAIL, OUTRAGE, HONE_CLAWS, DRAGON_PULSE, EARTH_POWER
 	; end
