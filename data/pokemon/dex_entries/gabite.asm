89e5080c01b361cc69d30e3d7e8b69c6d2a7b919
AMORAS81 <markamoras81@gmail.com>
2025-01-31 10:05:35 -0300

    gabite.asm

---
+++ data/pokemon/dex_entries/gabite.asm
@@ -0,0 +1,10 @@
+	db "VIBRATION@" ; species name
+	dw 307, 337 ; height, weight
+
+	db "It habitually digs"
+	next "up and hoards gems"
+	next "in its nest. Its"
+
+	page "loot is constantly"
+	next "targeted by"
+	next "thieves."
