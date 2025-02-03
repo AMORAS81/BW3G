80782a50c5501258bd5e48e7313dc7b987c602aa
AMORAS81 <markamoras81@gmail.com>
2025-02-03 16:23:48 -0300

    dvs.asm

--- data/trainers/dvs.asm
+++ data/trainers/dvs.asm
@@ -5,22 +5,22 @@ TrainerClassDVs:
 	dn  9,  8,  8,  8 ; YOUNGSTER_D
 	dn  5,  8,  8,  8 ; LASS
 	dn  5,  8,  8,  8 ; LASS_D
-	dn 13, 11, 13, 11 ; ACE_TRAINER_M
-	dn 13, 11, 13, 11 ; ACE_TRAINER_M_D
-	dn 11, 13, 13, 11 ; ACE_TRAINER_F
-	dn 11, 13, 13, 11 ; ACE_TRAINER_F_D
+	dn 13,  8, 12,  8 ; ACE_TRAINER_M
+	dn 13,  8, 12,  8 ; ACE_TRAINER_M_D
+	dn  7, 12, 12,  8 ; ACE_TRAINER_F
+	dn  7, 12, 12,  8 ; ACE_TRAINER_F_D
 	dn  9,  8,  8,  8 ; ARTIST
 	dn  9,  8,  8,  8 ; BACKERS_M
-	dn  6, 11, 11,  9 ; BACKERS_F
+	dn  6, 10, 10,  8 ; BACKERS_F
 	dn  9,  8,  8,  8 ; BACKPACKER_M
-	dn  6, 11, 11,  9 ; BACKPACKER_F
-	dn  6, 13,  9,  9 ; BAKER
+	dn  6, 10, 10,  8 ; BACKPACKER_F
+	dn  6, 13,  8,  8 ; BAKER
 	dn  9,  8,  8,  8 ; BLACKBELT_T
 	dn  9,  8,  8,  8 ; BATTLE_GIRL
 	dn  9,  8,  8,  8 ; BIKER
 	dn  9,  8,  8,  8 ; ROUGHNECK
 	dn  9,  8,  8,  8 ; CYCLIST_M
-	dn  6, 11, 11,  9 ; CYCLIST_F
+	dn  6, 10, 10,  8 ; CYCLIST_F
 	dn  9,  8,  8,  8 ; DANCER
 	dn  9,  8,  8,  8 ; DEPOT_AGENT
 	dn  9,  8,  8,  8 ; DOCTOR
@@ -28,82 +28,82 @@ TrainerClassDVs:
 	dn  9,  8,  8,  8 ; FISHER
 	dn  9,  8,  8,  8 ; FISHER_D
 	dn  9,  8,  8,  8 ; GENTLEMAN
-	dn  6,  9, 13,  9 ; GUITARIST
+	dn  6,  9, 12,  8 ; GUITARIST
 	dn  9,  8,  8,  8 ; HARLEQUIN
 	dn  7,  8,  8,  8 ; HEX_MANIAC
-	dn 11,  8,  8,  8 ; HIKER
-	dn 11,  8,  8,  8 ; HIKER_D
+	dn 10,  8,  8,  8 ; HIKER
+	dn 10,  8,  8,  8 ; HIKER_D
 	dn  9,  8,  8,  8 ; HOOPSTER
 	dn  9,  8,  8,  8 ; JANITOR
-	dn  6,  9, 13,  9 ; LADY
+	dn  6,  9, 12,  8 ; LADY
 	dn  9,  8,  8,  8 ; LINEBACKER
-	dn  6,  9, 13,  9 ; MAID
+	dn  6,  9, 12,  8 ; MAID
 	dn  9,  8,  8,  8 ; MUSICIAN
-	dn  7,  8,  8,  8 ; NURSERY_AIDE
+	dn  6,  8,  8,  8 ; NURSERY_AIDE
 	dn  6,  9, 12,  8 ; PARASOL_LADY
 	dn  9,  8,  8,  8 ; PILOT
 	dn  9,  8,  8,  8 ; POKEFAN_M
-	dn  6, 13,  9,  9 ; POKEFAN_F
+	dn  6, 13,  8,  8 ; POKEFAN_F
 	dn  9,  8,  8,  8 ; PKMN_BREEDER_M
-	dn  6, 11, 11,  9 ; PKMN_BREEDER_F
+	dn  6, 10, 10,  8 ; PKMN_BREEDER_F
 	dn  9,  8,  8,  8 ; PKMN_RANGER_M
-	dn  6, 11, 11,  9 ; PKMN_RANGER_F
-	dn 11, 11, 11, 11 ; POLICEMAN
+	dn  6, 10, 10,  8 ; PKMN_RANGER_F
+	dn  9,  8,  8,  8 ; POLICEMAN
 	dn  9,  8,  8,  8 ; PRESCHOOLER_M
-	dn  6, 11, 11,  9 ; PRESCHOOLER_F
+	dn  6, 10, 10,  8 ; PRESCHOOLER_F
 	dn  9,  8,  8,  8 ; PSYCHIC_M_T
-	dn  6,  9, 13,  9 ; PSYCHIC_F_T
+	dn  6,  9, 12,  8 ; PSYCHIC_F_T
 	dn  9,  8,  8,  8 ; RICH_BOY
 	dn  9,  8,  8,  8 ; SCHOOL_KID_M
-	dn  6, 11, 11,  9 ; SCHOOL_KID_F
+	dn  6, 10, 10,  8 ; SCHOOL_KID_F
 	dn  9,  8,  8,  8 ; SCIENTIST_M
-	dn  6, 11, 11,  9 ; SCIENTIST_F
+	dn  6, 10, 10,  8 ; SCIENTIST_F
 	dn  6, 10, 10,  8 ; SMASHER
-	dn  6,  9, 13,  9 ; SOCIALITE
+	dn  6,  9, 12,  8 ; SOCIALITE
 	dn  9,  8,  8,  8 ; SWIMMER_M
 	dn  9,  8,  8,  8 ; SWIMMER_M_D
-	dn  9,  8,  8,  8 ; SWIMMER_F
-	dn  9,  8,  8,  8 ; SWIMMER_F_D
-	dn  7,  9, 10,  8 ; TWINS
-	dn 13, 13, 13, 13 ; VETERAN_M
-	dn 13, 13, 13, 13 ; VETERAN_F
+	dn  7,  8,  8,  8 ; SWIMMER_F
+	dn  7,  8,  8,  8 ; SWIMMER_F_D
+	dn  6,  8, 10,  8 ; TWINS
+	dn 13,  8, 12,  8 ; VETERAN_M
+	dn  7, 12, 12,  8 ; VETERAN_F
 	dn  9,  8,  8,  8 ; WAITER
-	dn  6, 11, 11,  9 ; WAITRESS
-	dn 11,  9,  8,  8 ; WORKER
+	dn  6, 10, 10,  8 ; WAITRESS
+	dn 10,  8,  8,  8 ; WORKER
 	dn 13,  8, 10,  8 ; GRUNTM
-	dn  7, 13, 11,  9 ; GRUNTF
-	dn 13, 13, 13, 13 ; MARLON
-	dn 13, 13, 13, 13 ; SHAUNTAL
-	dn 13, 13, 13, 13 ; BURGH
-	dn 13, 13, 13, 13 ; ROXIE
-	dn 15, 15, 15, 15 ; CHEREN
-	dn 13, 13, 13, 13 ; CILAN
-	dn 13, 13, 13, 13 ; SKYLA
-	dn 15, 15, 15, 15 ; DRAYDEN
-	dn 15, 15, 15, 15 ; MARSHAL
-	dn 15, 15, 15, 15 ; GRIMSLEY
-	dn 15, 15, 15, 15 ; ELESA
-	dn 15, 15, 15, 15 ; COLRESS
-	dn 13, 13, 13, 13 ; JUNIPER
-	dn 13, 13, 13, 13 ; BIANCA
-	dn 13, 13, 13, 13 ; GIALLO
-	dn 13, 13, 13, 13 ; BRONIUS
-	dn 13, 13, 13, 13 ; RYOKU
-	dn 13, 13, 13, 13 ; GORM
-	dn 13, 13, 13, 13 ; MYSTERYMAN
-	dn 13, 13, 13, 13 ; ZINZOLIN
-	dn 15, 15, 15, 15 ; GENESIS
-	dn 13, 13, 13, 13 ; INFER
-	dn 13, 13, 13, 13 ; INFER_SAGE
-	dn 13, 13, 13, 13 ; INFER2
-	dn 15, 15, 15, 15 ; ALDER
-	dn 15, 15, 15, 15 ; IRIS
-	dn 15, 15, 15, 15 ; CAITLIN
-	dn 15, 15, 15, 15 ; CYNTHIA
-	dn 15, 15, 15, 15 ; HUGH
-	dn 15, 15, 15, 15 ; ROOD
-	dn 15, 15, 15, 15 ; N
-	dn 15, 15, 15, 15 ; HILBERT
-	dn 15, 15, 15, 15 ; HILDA
-	dn 15, 15, 15, 15 ; NATE
-	dn 15, 15, 15, 15 ; ROSA
+	dn  7, 14, 10,  8 ; GRUNTF
+	dn 10,  9, 10,  9 ; MARLON
+	dn 10,  9, 10,  9 ; SHAUNTAL
+	dn 10,  9, 10,  9 ; BURGH
+	dn 10,  9, 10,  9 ; ROXIE
+	dn 10,  9, 10,  9 ; CHEREN
+	dn 10,  9, 10,  9 ; CILAN
+	dn 10,  9, 10,  9 ; SKYLA
+	dn 10,  9, 10,  9 ; DRAYDEN
+	dn 10,  9, 10,  9 ; MARSHAL
+	dn 10,  9, 10,  9 ; GRIMSLEY
+	dn 10,  9, 10,  9 ; ELESA
+	dn 10,  9, 10,  9 ; COLRESS
+	dn 10,  9, 10,  9 ; JUNIPER
+	dn 10,  9, 10,  9 ; BIANCA
+	dn 10,  9, 10,  9 ; GIALLO
+	dn 10,  9, 10,  9 ; BRONIUS
+	dn 10,  9, 10,  9 ; RYOKU
+	dn 10,  9, 10,  9 ; GORM
+	dn 10,  9, 10,  9 ; MYSTERYMAN
+	dn 10,  9, 10,  9 ; ZINZOLIN
+	dn 10,  9, 10,  9 ; GENESIS
+	dn 10,  9, 10,  9 ; INFER
+	dn 10,  9, 10,  9 ; INFER_SAGE
+	dn 10,  9, 10,  9 ; INFER2
+	dn 10,  9, 10,  9 ; ALDER
+	dn 10,  9, 10,  9 ; IRIS
+	dn 10,  9, 10,  9 ; CAITLIN
+	dn 10,  9, 10,  9 ; CYNTHIA
+	dn 10,  9, 10,  9 ; HUGH
+	dn 10,  9, 10,  9 ; ROOD
+	dn 10,  9, 10,  9 ; N
+	dn 10,  9, 10,  9 ; HILBERT
+	dn 10,  9, 10,  9 ; HILDA
+	dn 10,  9, 10,  9 ; NATE
+	dn 10,  9, 10,  9 ; ROSA
