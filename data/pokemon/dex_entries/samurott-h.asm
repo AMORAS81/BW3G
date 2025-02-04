11c800096da025e80ff37b686e112c4b5b05c2ec
AMORAS81 <markamoras81@gmail.com>
2025-02-04 17:42:28 -0300

    Update and rename snubbull.asm to samurott-h.asm

---
+++ data/pokemon/dex_entries/samurott-h.asm
@@ -0,0 +1,10 @@
+	db "FORMIDABLE@" ; species name
+	dw 411, 1283 ; height, weight
+
+	db   "Hard of heart and"
+	next "deft of blade. Its"
+	next "turbulent blows"
+
+	page "crash into foes"
+	next "like ceaseless"
+	next "pounding waves.@"
