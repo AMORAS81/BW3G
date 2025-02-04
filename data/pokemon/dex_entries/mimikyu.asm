fed9451ad36a72a22d685fdd55d1c9d2c96ec18b
AMORAS81 <markamoras81@gmail.com>
2025-02-04 17:47:26 -0300

    Update and rename granbull.asm to mimikyu.asm

---
+++ data/pokemon/dex_entries/mimikyu.asm
@@ -0,0 +1,10 @@
+	db "DISGUISE@" ; species name
+	dw 407, 1070 ; height, weight
+
+	db   "Its appearance is"
+	next "actually unknown."
+	next "A scholar who saw"
+
+	page "under its rag was"
+	next "woverwhelmed and"
+	next "died from shock.@"
