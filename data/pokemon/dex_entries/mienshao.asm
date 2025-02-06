f7ff7a806c0671a5a6222b3bcb6b91cf18ece9dc
AMORAS81 <markamoras81@gmail.com>
2025-02-06 12:18:32 -0300

    Update and rename medicham.asm to mienshao.asm

---
+++ data/pokemon/dex_entries/mienshao.asm
@@ -0,0 +1,10 @@
+	db "MARTIAL ARTS@" ; species name
+	dw 407, 783 ; height, weight
+
+	db "They use the long"
+	next "on its arms like"
+	next "whips, it launches"
+
+	page "into combo attacks"
+	next "that, once started"
+	next ",no one can stop.@"
